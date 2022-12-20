9a5f3365d667b95acdf989019eca3dfc072d9a1e
Rizwan Ali <rizwan.ali@qbatch.com>
2022-12-20 11:40:05 +0500

    Login and Forget Password Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+// import ForgetPassword from './components/ForgetPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="backgroundcls">
+        <div className="Rectangle1"></div>
+        <div className="Rectangle2"></div>
+        <Login />
+        {/* <ForgetPassword /> */}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ForgetPassword.js
@@ -0,0 +1,34 @@
+import './ForgetPasswordStyles.css';
+import React from 'react';
+import '@fontsource/lato';
+import myimage from '../assets/Qbatch_logo-removebg-preview.png';
+
+export default function ForgetPassword() {
+  return (
+    <>
+      <form className="inner">
+        <img
+          className="Qbatch_logo_remove_bg_preview1"
+          src={myimage}
+          alt="Logo Icon"
+        />
+        <div className="ForgotPasswordHeading">Forgot Password?</div>
+        <p className="ForgetPasswordInst">
+          Please enter your qbatch email address and we'll send you instructions
+          to reset your password.
+        </p>
+        <label className="Email">Email</label>
+        <input
+          className="EmailField form-control"
+          placeholder="name@qbatch.com"
+        />
+        <button className="SendResetInst">Send Reset Instructions</button>
+        {/* <div className="line73"></div> */}
+        <div className="or">or</div>
+        {/* <div className="line74"></div> */}
+        <div className="returntotxt">Return to</div>
+        <div className="btn sign_in">Sign In</div>
+      </form>
+    </>
+  );
+}
